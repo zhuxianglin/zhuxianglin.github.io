6ca5b324a8d3ea5a3d4d2aadce724d9c2927d14b
iCauchy <mail_wcl@163.com>
2018-03-30 12:20:49 +0800

    add some text

--- javascript/mainpage.js
+++ javascript/mainpage.js
@@ -8,28 +8,28 @@ function startTime(){
 	
 	if (h >0 && h < 7)
 	{
-		document.getElementById('time').innerHTML=h+":"+m+":"+s + "   晚安";
+		document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   晚安";
 	}
 		else if(h >=7 && h <11)
 		{
-			document.getElementById('time').innerHTML=h+":"+m+":"+s + "   上午好";
+			document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   上午好";
 		}
 			else if(h >=11 && h <14)
 			{
-				document.getElementById('time').innerHTML=h+":"+m+":"+s + "   中午好";
+				document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   中午好";
 				
 			}
 				else if(h >=14 && h <19)
 				{
-					document.getElementById('time').innerHTML=h+":"+m+":"+s + "   下午好";
+					document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   下午好";
 				}
 					else if(h >=19 && h <22)
 					{
-						document.getElementById('time').innerHTML=h+":"+m+":"+s + "   晚上好";
+						document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   晚上好";
 					}
 						else
 						{
-							document.getElementById('time').innerHTML=h+":"+m+":"+s + "   早点休息";
+							document.getElementById('time').innerHTML="香霖，现在是北京时间："+h+":"+m+":"+s + "   早点休息";
 						}
 	t=setTimeout(function(){startTime()},500);
 }
